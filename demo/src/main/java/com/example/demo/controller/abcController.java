1c2892aab99502c08ed0d14d4e65c35ba8493d40
xcd <1132521981@qq.com>
2019-12-13 15:14:28 +0800

    小马哥牛皮1

--- demo/src/main/java/com/example/demo/controller/abcController.java
+++ demo/src/main/java/com/example/demo/controller/abcController.java
@@ -3,7 +3,7 @@ package com.example.demo.controller;
 public class abcController {
     @Override
     public boolean equals(Object obj) {
-        System.out.println("小马哥v587,工好好学习,天天向上");
+        System.out.println("小马哥v587");
         return super.equals(obj);
     }
 }
