30b13d1d64e1d8f660aff972ef5b7c4eb238eb7b
xcd <1132521981@qq.com>
2019-12-13 15:34:53 +0800

    测试1

--- demo/src/main/java/com/example/demo/controller/abcController.java
+++ demo/src/main/java/com/example/demo/controller/abcController.java
@@ -3,7 +3,7 @@ package com.example.demo.controller;
 public class abcController {
     @Override
     public boolean equals(Object obj) {
-        System.out.println("小马哥v587111");
+        System.out.println("小马哥v58711111");
         return super.equals(obj);
     }
 }
