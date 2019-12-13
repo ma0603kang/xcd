598a4e7da2270e4192d1663668d6464e4c45a3b2
xcd <1132521981@qq.com>
2019-12-13 15:37:00 +0800

    v5

--- demo/src/main/java/com/example/demo/controller/abcController.java
+++ demo/src/main/java/com/example/demo/controller/abcController.java
@@ -3,7 +3,7 @@ package com.example.demo.controller;
 public class abcController {
     @Override
     public boolean equals(Object obj) {
-        System.out.println("小马哥v587");
+        System.out.println("小马哥威武霸气！");
         return super.equals(obj);
     }
 }
