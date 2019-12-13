56aa8e42e7adb85812a0490eb610c76831435309
xcd <1132521981@qq.com>
2019-12-13 15:54:41 +0800

    这是改的主线

--- demo/src/main/java/com/example/demo/controller/abcController.java
+++ demo/src/main/java/com/example/demo/controller/abcController.java
@@ -3,7 +3,7 @@ package com.example.demo.controller;
 public class abcController {
     @Override
     public boolean equals(Object obj) {
-        System.out.println("小马哥溜的一批！");
+        System.out.println("小马哥溜的一批！，这是改的主线");
         return super.equals(obj);
     }
 }
