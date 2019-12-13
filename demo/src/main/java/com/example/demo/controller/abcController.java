22645df42465fa603e4494f4173ddccbdcd78ebb
xcd <1132521981@qq.com>
2019-12-13 15:48:37 +0800

    分支

--- demo/src/main/java/com/example/demo/controller/abcController.java
+++ demo/src/main/java/com/example/demo/controller/abcController.java
@@ -3,7 +3,7 @@ package com.example.demo.controller;
 public class abcController {
     @Override
     public boolean equals(Object obj) {
-        System.out.println("小马哥威武霸气！");
+        System.out.println("小马哥溜的一批！");
         return super.equals(obj);
     }
 }
